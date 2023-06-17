ba968de615def2fbec0ab8eadf6f5d4b16a02c80
Mike S <kwandike@gmail.com>
2023-06-17 17:57:31 +0300

    "setup basic class and values"

---
+++ src/main/java/Comparing.java
@@ -0,0 +1,18 @@
+import javax.sound.midi.Soundbank;
+
+public class Comparing {
+
+    public static void main(String[] argv) {
+
+        int value1 = 2;
+        int value2 = 5;
+        int value3 = 1;
+        int value4 = 8;
+
+
+        int sum1 = value1 + value2;
+        int sum2 = value3 + value4;
+
+
+    }
+}
