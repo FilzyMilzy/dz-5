3ea57e2639097dbb7d011e95313df6cd30bce4c1
Mike S <kwandike@gmail.com>
2023-06-17 18:23:03 +0300

    Implemented core assignment code

--- src/main/java/Comparing.java
+++ src/main/java/Comparing.java
@@ -13,6 +13,31 @@ public class Comparing {
         int sum1 = value1 + value2;
         int sum2 = value3 + value4;
 
+        System.out.println("First condition output is:");
+        if(sum1>sum2){ //according to friday slack update
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        sum1++;
+        sum2-=2;
+
+        System.out.println("Second condition output is: ");
+        if(sum1<sum2){//according to friday slack update
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        System.out.println("Third condition output is: ");
+        if(sum1%2 == 0 || sum2%2 == 0){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
 
     }
+
 }
+
